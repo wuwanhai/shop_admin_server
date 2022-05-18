1b623e95b589bd002ce884cc262cff1517e9ff4b
wuwanhai <haiwanwu@outlook.com>
2022-05-18 15:30:24 +0800

    增加中间件和路由处理

--- app.js
+++ app.js
@@ -1,9 +1,48 @@
 const Koa = require('koa');
 
-const app = new Koa();
+const path = require('path');
 
-app.use(async (ctx) => {
-    ctx.body = '测试热更新!'
-})
+const app = new Koa();
+const onerror = require('koa-onerror');
+const koaBody = require('koa-body');
+const json = require('koa-json');
+const logger = require('koa-logger');
+
+const responseFormatter = require('./middleware/response_formatter');
+const { apiPrefix } = require('./config/index');
+const routers = require('./routers/index');
+
+// koa的错误处理程序hack
+onerror(app);
+
+// middlewares
+app.use(koaBody({
+  multipart: true, // 支持文件上传
+  formidable: {
+    formidable: {
+      uploadDir: path.join(__dirname, 'public/upload/'), // 设置文件上传目录
+      keepExtensions: true, // 保持文件的后缀
+      maxFieldsSize: 2 * 1024 * 1024, // 文件上传大小
+      onFileBegin: (name, file) => { // 文件上传前的设置
+        console.log(`name: ${name}`);
+        console.log(file);
+      },
+    },
+  },
+}));
+app.use(json());
+app.use(logger());
+
+// response formatter
+app.use(responseFormatter(apiPrefix));
+
+// routers
+app.use(routers.routes()).use(routers.allowedMethods());
+
+// 监听error
+app.on('error', (err, ctx) => {
+  // 在这里可以对错误信息进行一些处理，生成日志等。
+  console.error('server error', err, ctx);
+});
 
 app.listen(8080)
---
+++ routers/index.js
@@ -0,0 +1,12 @@
+const router = require('koa-router')();
+const { apiPrefix } = require('../config/index');
+
+const blog = require('./blog');
+const category = require('./category');
+
+router.prefix(apiPrefix);
+
+router.use('/blogs', blog.routes(), blog.allowedMethods());
+router.use('/categories', category.routes(), category.allowedMethods());
+
+module.exports = router;
